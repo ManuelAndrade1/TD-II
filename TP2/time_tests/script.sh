051ba4f806cb67db22965e53d6ee31737a663092
ManuelAndrade1 <themanuelandrade@gmail.com>
2023-09-08 12:33:43 -0300

    Modified script so it runs all three steps at once

--- TP2/time_tests/script.sh
+++ TP2/time_tests/script.sh
@@ -3,9 +3,23 @@
 program="$1"
 uinp="$2"
 uout="$3"
-count="$4"
-fname="$5"
+fname="$4"
+step1="_1step"
+step5="_5step"
+step10="_10step"
+t=".txt"
 
+f="$fname$step1$t"
 for ((i=0; i < 10; i++)); do 
-    ( time ./"$program" "$uinp" "$uout" "$count" ) >> "$fname" 2>&1;
+    ( time ./"$program" "$uinp" "$uout" "1" ) >> "$f" 2>&1;
+done;
+
+f="$fname$step5$t"
+for ((i=0; i < 10; i++)); do 
+    ( time ./"$program" "$uinp" "$uout" "5" ) >> "$f" 2>&1;
+done;
+
+f="$fname$step10$t"
+for ((i=0; i < 10; i++)); do 
+    ( time ./"$program" "$uinp" "$uout" "10" ) >> "$f" 2>&1;
 done;
