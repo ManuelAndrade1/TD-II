c1956f47b58b195fe9d3a5734b539218b9bfb119
ManuelAndrade1 <themanuelandrade@gmail.com>
2023-09-15 20:32:10 -0300

    Extended exercises file

--- EJERCICIOS_C/ej1.c
+++ EJERCICIOS_C/ej1.c
@@ -22,50 +22,18 @@ struct node {
 
 struct list {
     struct node* first;
+    int size;
 };
 
-void addLast(struct list* ls, int v) {
-    // Agrega un nodo al final de la lista
-    struct node* new = (struct node*) malloc(sizeof(struct node));
-    new->next = 0; // No apunta a nadie dado que es el ultimo
-    new->value = v;
+void addLast(struct list* ls, int v);
+void deleteList(struct list* ls);
+void printList(struct list* ls);
+struct list** createLists();
+struct list** createListsBis();
+void deleteTest(struct list** ls);
 
-    if (ls->first == 0) { // Caso base: Lista vacia
-        ls->first = new;
-        return;
-    }
-    struct node* curr = ls->first;
-    while (curr->next != 0) {
-        curr = curr->next;
-    }
-    // Al salir del ciclo, curr es un puntero al ultimo nodo de la lista
-    curr->next = new; // Agrego el ultimo al final
-    return;
-}
-void deleteList(struct list* ls) {
-    // Borra la lista - Libera la memoria
-    if (ls->first == 0) return;
-    struct node* curr = ls->first;
-    while (curr != 0) { // Uso solo curr porque quiero iterar hasta el ultimo INCLUSIVE
-        struct node* tmp = curr;
-        curr = curr->next;
-        free(tmp);
-    }
-    free(ls);
-    return;
-}
-void printList(struct list* ls) {
-    if (ls->first == 0) return;
+int compareLists(struct list* ls, int* arr, int len);
 
-    printf("[ ");
-    struct node* curr = ls->first;
-    while(curr != 0) {
-        printf("%i ", curr->value);
-        curr = curr->next;
-    }
-    printf("]\n");
-    return;
-}
 float average(struct list* ls) {
     // COMPLETAR
     /*
@@ -126,12 +94,193 @@ void lastToFirst(struct list* ls) {
 }
 
 int main() {
-    struct list* ls = (struct list*) malloc(sizeof(struct list));
-    ls->first = 0;
-    for (int i = 0; i < 12; i++) {
-        addLast(ls, i);
-    }
-    printList(ls);
-    deleteList(ls);
+    struct list** ls;
+    ls = createLists();
+    printf("First samples:\n");
+    printList(ls[0]);
+    printList(ls[1]);
+    printList(ls[2]);
+    printList(ls[3]);
+    printf("\n\n");   
+    deleteTest(ls);
+    ls = createListsBis(ls);
+    printf("Second samples:\n");
+    printList(ls[0]);
+    printList(ls[1]);
+    printList(ls[2]);
+    printList(ls[3]); 
+    printf("\n\n");
+    deleteTest(ls);
+    // TEST removeNode
+    // ls = createLists();
+    // printf("Testing removeNode()...\n\n");
+    // removeNode(ls[0], 2);
+    // removeNode(ls[1], 1);
+    // removeNode(ls[2], 5);
+    // removeNode(ls[3], 4);
+    // int data[5] = {1, 2, 3, 2, 5};
+    // int data2[5] = {3, 3, 3, 3, 5};
+    // int data3[5] = {0, 1, 4, 3, 16};
+    // int data4[6] = {1, 1, 1, 1, 1, 1};
+    // int test_res[4] = {compareLists(ls[0], data, 5), compareLists(ls[1], data2, 5), 
+    //                     compareLists(ls[2], data3, 5), compareLists(ls[3], data4, 6)};
+    // for (int i = 0; i < 3; i++) {
+    //     if (test_res[i]) printf("Test successful!\n");
+    //     else printf("Test failed!\n"); 
+    // }
+    // deleteTest(ls);
+
+    // TEST removeNodes
+    // ls = createLists();
+    // printf("Testing removeNodes()...\n\n");
+    // removeNodes(ls[0], 2);
+    // removeNodes(ls[1], 3);
+    // removeNodes(ls[2], 16);
+    // removeNodes(ls[3], 1);
+    // int n_data[3] = {1, 3, 5};
+    // int n_data2[2] = {1, 5};
+    // int n_data3[5] = {0, 1, 4, 3, 5};
+    // int n_data4[0] = {};
+    // int test_res[4] = {compareLists(ls[0], n_data, 3), compareLists(ls[1], n_data2, 2), 
+    //                     compareLists(ls[2], n_data3, 5), compareLists(ls[3], n_data4, 0)};
+    // for (int i = 0; i < 3; i++) {
+    //     if (test_res[i]) printf("Test successful!\n");
+    //     else printf("Test failed!\n"); 
+    // }
+    // deleteTest(ls);
+
+    // TEST firstToLast
+    // ls = createListsBis();
+    // printf("Testing firstToLast()...\n\n");
+    // firstToLast(ls[0]);
+    // firstToLast(ls[1]);
+    // firstToLast(ls[2]);
+    // firstToLast(ls[3]);
+    // int f_data[1] = {1};
+    // int f_data2[2] = {4, 3};
+    // int f_data3[8] = {2, 3, 4, 5, 6, 7, 8, 1};
+    // int f_data4[0] = {};
+    // int test_res[4] = {compareLists(ls[0], f_data, 1), compareLists(ls[1], f_data2, 2), 
+    //                     compareLists(ls[2], f_data3, 8), compareLists(ls[3], f_data4, 0)};
+    // for (int i = 0; i < 3; i++) {
+    //     if (test_res[i]) printf("Test successful!\n");
+    //     else printf("Test failed!\n"); 
+    // }
+    // deleteTest(ls);
+
+    // TEST lastToFirst
+    // ls = createListsBis();   
+    // printf("Testing lastToFirst()...\n\n");
+    // lastToFirst(ls[0]);
+    // lastToFrst(ls[1]);
+    // lastToFirst(ls[2]);
+    // lastToFirst(ls[3]);
+    // int l_data[1] = {1};
+    // int l_data2[2] = {4, 3};
+    // int l_data3[8] = {8, 1, 2, 3, 4, 5, 6, 7};
+    // int l_data4[0] = {};
+    // int test_res[4] = {compareLists(ls[0], l_data, 1), compareLists(ls[1], l_data2, 2), 
+    //                     compareLists(ls[2], l_data3, 8), compareLists(ls[3], l_data4, 0)};
+    // for (int i = 0; i < 3; i++) {
+    //     if (test_res[i]) printf("Test successful!\n");
+    //     else printf("Test failed!\n"); 
+    // }
+    // deleteTest(ls);
+    deleteList(ls[0]);
+    deleteList(ls[1]);
+    deleteList(ls[2]);
+    deleteList(ls[3]);
     return 0;
+}
+
+
+void addLast(struct list* ls, int v) {
+    // Agrega un nodo al final de la lista
+    struct node* new = (struct node*) malloc(sizeof(struct node));
+    new->next = 0; // No apunta a nadie dado que es el ultimo
+    new->value = v;
+    ls->size++; // Actualizo size de la lista
+
+    if (ls->first == 0) { // Caso base: Lista vacia
+        ls->first = new;
+        return;
+    }
+    struct node* curr = ls->first;
+    while (curr->next != 0) {
+        curr = curr->next;
+    }
+    // Al salir del ciclo, curr es un puntero al ultimo nodo de la lista
+    curr->next = new; // Agrego el ultimo al final
+    return;
+}
+
+void deleteList(struct list* ls) {
+    // Borra la lista - Libera la memoria
+    if (ls->first == 0) return;
+    struct node* curr = ls->first;
+    while (curr != 0) { // Uso solo curr porque quiero iterar hasta el ultimo INCLUSIVE
+        struct node* tmp = curr;
+        curr = curr->next;
+        free(tmp);
+    }
+    free(ls);
+    return;
+}
+
+void printList(struct list* ls) {
+    if (ls->first == 0) return;
+
+    printf("[ ");
+    struct node* curr = ls->first;
+    while(curr != 0) {
+        printf("%i ", curr->value);
+        curr = curr->next;
+    }
+    printf("]\n");
+    return;
+}
+
+struct list** createLists() {
+    // Crea 4 listas para testing
+    struct list** lists = (struct list**) malloc(sizeof(struct list*) * 4);
+    for (int i = 0; i < 6; i++) {
+        if (i % 2 == 0) {
+            addLast(lists[0], 2);
+            addLast(lists[1], 3);
+            addLast(lists[2], i*i);
+        }
+        else {
+            addLast(lists[0], i);
+            addLast(lists[1], i);
+            addLast(lists[2], i);
+        }
+        addLast(lists[3], 1);
+    }
+    return lists;
+}
+
+struct list** createListsBis() {
+    // Creo 4 listas para firstToLast y lastToFirst
+    struct list** lists = (struct list**) malloc(sizeof(struct list*) * 4);
+    addLast(lists[0], 1);
+    addLast(lists[1], 3);
+    addLast(lists[1], 4);
+    for (int i = 0; i < 8; i++) addLast(lists[2], i+1);
+    return lists;
+}
+
+void deleteTest(struct list** ls) {
+    for (int i = 0; i < 4; i++) free(ls[i]);
+    free(ls);
+}
+
+int compareLists(struct list* ls, int* arr, int len) {
+    // Devuelve 1 (true) si las listas son iguales, 0 (false) si las listas son diff.
+    struct node* curr = ls->first; // Nodo que itera por toda la lista
+    if (ls->size != len) return 0; // La lista no tiene el tamano deseado
+    for (int i = 0; i < len; i++) {
+        if (curr->value != arr[i]) return 0; // Los valores no coinciden
+        curr = curr->next;
+    }
+    return 1; // Las listas son iguales
 }
