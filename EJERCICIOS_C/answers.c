e0990aaa1f1ddbc1115827e5a3b8ce1ba0a856b7
ManuelAndrade1 <themanuelandrade@gmail.com>
2023-09-15 22:01:11 -0300

    Finished exercise with test cases

--- EJERCICIOS_C/answers.c
+++ EJERCICIOS_C/answers.c
@@ -57,6 +57,29 @@ void removeNode(struct list* ls, int v) {
     La funcion debe ELIMINAR (borrar de la lista y liberar memoria) el primer nodo
     que coincida con el valor pasado como parametro. Si ninguno coincide, la lista queda igual.
     */
+   if (ls->first == 0) return;
+   if (ls->first->value == v) {
+    struct node* aux = ls->first;
+    ls->first = aux->next;
+    free(aux);
+    ls->size--;
+    return;
+   }
+   struct node* prev = ls->first;
+   struct node* curr = ls->first->next;
+   while (curr != 0) {
+    if (curr->value == v) {
+        struct node* aux = curr;
+        curr = curr->next;
+        prev->next = curr;
+        free(aux);
+        ls->size--;
+        return;
+    }
+    prev = curr;
+    curr = curr->next;
+   }
+   return;
 }
 
 void removeNodes(struct list* ls, int v) {
@@ -67,6 +90,28 @@ void removeNodes(struct list* ls, int v) {
     cuyo valor coinida con el entero pasado como parametro. Si ninguno coincide, la
     lista queda igual. 
     */
+   if (ls->first == 0) return;
+   struct node* prev = ls->first;
+   struct node* curr = ls->first->next;
+   while (curr != 0) {
+    if (curr->value == v) {
+        struct node* aux = curr;
+        curr = curr->next;
+        prev->next = curr;
+        free(aux);
+        ls->size--;
+    }
+    else {
+        prev = curr;
+        curr = curr->next;
+    }
+   }
+   if (ls->first->value == v) {
+    struct node* aux = ls->first;
+    ls->first = aux->next;
+    free(aux);
+    ls->size--;
+   }
    return;
 }
 
@@ -77,6 +122,13 @@ void firstToLast(struct list* ls) {
     La funcion debe "mover" el primer nodo de la lista al final de la lista.
     Si la lista esta vacia, o si la lista contiene un solo nodo no debe hacer nada.
     */
+   if (ls->first == 0 || ls->size == 1) return;
+    struct node* first = ls->first;
+    struct node* curr = first;
+    ls->first = first->next;
+   while (curr->next != 0) curr = curr->next;
+   curr->next = first;
+   first->next = 0;  
    return;
 }
 
@@ -87,6 +139,16 @@ void lastToFirst(struct list* ls) {
     La funcion debe "mover" el ultimo nodo de la lista al principio de la lista.
     Si la lista esta vacia, o si la lista contiene un solo nodo no debe hacer nada.
     */
+    if (ls->first == 0 || ls->size == 1) return;
+    struct node* curr = ls->first->next;
+    struct node* prev = ls->first;
+    while (curr->next != 0) {
+        prev = curr;
+        curr = curr->next;
+    }
+    prev->next = 0;
+    curr->next = ls->first;
+    ls->first = curr;
     return;
 }
 
@@ -110,91 +172,90 @@ int main() {
     deleteTest(ls);
 
     // TEST findValue
-    // ls = createLists();
-    // printf("Testing findValue()...\n\n");
-    // testFindValue(ls);
-    // deleteTest(ls);
+    ls = createLists();
+    printf("Testing findValue()...\n\n");
+    testFindValue(ls);
+    deleteTest(ls);
 
     // TEST removeNode
-    // ls = createLists();
-    // printf("Testing removeNode()...\n\n");
-    // removeNode(ls[0], 2);
-    // removeNode(ls[1], 1);
-    // removeNode(ls[2], 5);
-    // removeNode(ls[3], 4);
-    // int data[5] = {1, 2, 3, 2, 5};
-    // int data2[5] = {3, 3, 3, 3, 5};
-    // int data3[5] = {0, 1, 4, 3, 16};
-    // int data4[6] = {1, 1, 1, 1, 1, 1};
-    // int test_res[4] = {compareLists(ls[0], data, 5), compareLists(ls[1], data2, 5), 
-    //                     compareLists(ls[2], data3, 5), compareLists(ls[3], data4, 6)};
-    // for (int i = 0; i < 3; i++) {
-    //     if (test_res[i]) printf("Test successful!\n");
-    //     else printf("Test failed!\n"); 
-    // }
-    // printf("\n\n");
-    // deleteTest(ls);
+    ls = createLists();
+    printf("Testing removeNode()...\n\n");
+    removeNode(ls[0], 2);
+    removeNode(ls[1], 1);
+    removeNode(ls[2], 5);
+    removeNode(ls[3], 4);
+    int data[5] = {1, 2, 3, 2, 5};
+    int data2[5] = {3, 3, 3, 3, 5};
+    int data3[5] = {0, 1, 4, 3, 16};
+    int data4[6] = {1, 1, 1, 1, 1, 1};
+    int test_res[4] = {compareLists(ls[0], data, 5), compareLists(ls[1], data2, 5), 
+                        compareLists(ls[2], data3, 5), compareLists(ls[3], data4, 6)};
+    for (int i = 0; i < 3; i++) {
+        if (test_res[i]) printf("Test successful!\n");
+        else printf("Test failed!\n"); 
+    }
+    printf("\n\n");
+    deleteTest(ls);
 
     // TEST removeNodes
-    // ls = createLists();
-    // printf("Testing removeNodes()...\n\n");
-    // removeNodes(ls[0], 2);
-    // removeNodes(ls[1], 3);
-    // removeNodes(ls[2], 16);
-    // removeNodes(ls[3], 1);
-    // int n_data[3] = {1, 3, 5};
-    // int n_data2[2] = {1, 5};
-    // int n_data3[5] = {0, 1, 4, 3, 5};
-    // int n_data4[0] = {};
-    // int n_test_res[4] = {compareLists(ls[0], n_data, 3), compareLists(ls[1], n_data2, 2), 
-    //                     compareLists(ls[2], n_data3, 5), compareLists(ls[3], n_data4, 0)};
-    // for (int i = 0; i < 3; i++) {
-    //     if (n_test_res[i]) printf("Test successful!\n");
-    //     else printf("Test failed!\n"); 
-    // }
-    // printf("\n\n");
-    // deleteTest(ls);
+    ls = createLists();
+    printf("Testing removeNodes()...\n\n");
+    removeNodes(ls[0], 2);
+    removeNodes(ls[1], 3);
+    removeNodes(ls[2], 16);
+    removeNodes(ls[3], 1);
+    int n_data[3] = {1, 3, 5};
+    int n_data2[2] = {1, 5};
+    int n_data3[5] = {0, 1, 4, 3, 5};
+    int n_data4[0] = {};
+    int n_test_res[4] = {compareLists(ls[0], n_data, 3), compareLists(ls[1], n_data2, 2), 
+                        compareLists(ls[2], n_data3, 5), compareLists(ls[3], n_data4, 0)};
+    for (int i = 0; i < 3; i++) {
+        if (n_test_res[i]) printf("Test successful!\n");
+        else printf("Test failed!\n"); 
+    }
+    printf("\n\n");
+    deleteTest(ls);
 
     // TEST firstToLast
-    // ls = createListsBis();
-    // printf("Testing firstToLast()...\n\n");
-    // firstToLast(ls[0]);
-    // firstToLast(ls[1]);
-    // firstToLast(ls[2]);
-    // firstToLast(ls[3]);
-    // int f_data[1] = {1};
-    // int f_data2[2] = {4, 3};
-    // int f_data3[8] = {2, 3, 4, 5, 6, 7, 8, 1};
-    // int f_data4[0] = {};
-    // int f_test_res[4] = {compareLists(ls[0], f_data, 1), compareLists(ls[1], f_data2, 2), 
-    //                     compareLists(ls[2], f_data3, 8), compareLists(ls[3], f_data4, 0)};
-    // for (int i = 0; i < 3; i++) {
-    //     if (f_test_res[i]) printf("Test successful!\n");
-    //     else printf("Test failed!\n"); 
-    // }
-    // printf("\n\n");
-    // deleteTest(ls);
+    ls = createListsBis();
+    printf("Testing firstToLast()...\n\n");
+    firstToLast(ls[0]);
+    firstToLast(ls[1]);
+    firstToLast(ls[2]);
+    firstToLast(ls[3]);
+    int f_data[1] = {1};
+    int f_data2[2] = {4, 3};
+    int f_data3[8] = {2, 3, 4, 5, 6, 7, 8, 1};
+    int f_data4[0] = {};
+    int f_test_res[4] = {compareLists(ls[0], f_data, 1), compareLists(ls[1], f_data2, 2), 
+                        compareLists(ls[2], f_data3, 8), compareLists(ls[3], f_data4, 0)};
+    for (int i = 0; i < 3; i++) {
+        if (f_test_res[i]) printf("Test successful!\n");
+        else printf("Test failed!\n"); 
+    }
+    printf("\n\n");
+    deleteTest(ls);
 
     // TEST lastToFirst
-    // ls = createListsBis();   
-    // printf("Testing lastToFirst()...\n\n");
-    // lastToFirst(ls[0]);
-    // lastToFirst(ls[1]);
-    // lastToFirst(ls[2]);
-    // lastToFirst(ls[3]);
-    // int l_data[1] = {1};
-    // int l_data2[2] = {4, 3};
-    // int l_data3[8] = {8, 1, 2, 3, 4, 5, 6, 7};
-    // int l_data4[0] = {};
-    // int l_test_res[4] = {compareLists(ls[0], l_data, 1), compareLists(ls[1], l_data2, 2), 
-    //                     compareLists(ls[2], l_data3, 8), compareLists(ls[3], l_data4, 0)};
-    // for (int i = 0; i < 3; i++) {
-    //     if (l_test_res[i]) printf("Test successful!\n");
-    //     else printf("Test failed!\n"); 
-    // }
-    // printf("\n\n");
-    // deleteTest(ls);
-    
+    ls = createListsBis();   
+    printf("Testing lastToFirst()...\n\n");
+    lastToFirst(ls[0]);
+    lastToFirst(ls[1]);
+    lastToFirst(ls[2]);
+    lastToFirst(ls[3]);
+    int l_data[1] = {1};
+    int l_data2[2] = {4, 3};
+    int l_data3[8] = {8, 1, 2, 3, 4, 5, 6, 7};
+    int l_data4[0] = {};
+    int l_test_res[4] = {compareLists(ls[0], l_data, 1), compareLists(ls[1], l_data2, 2), 
+                        compareLists(ls[2], l_data3, 8), compareLists(ls[3], l_data4, 0)};
+    for (int i = 0; i < 3; i++) {
+        if (l_test_res[i]) printf("Test successful!\n");
+        else printf("Test failed!\n"); 
+    }
+    printf("\n\n");
+    deleteTest(ls);
     return 0;
 }
 
