4438d92d8ea6c4c08044dc1466c2ef1b4a1ce4c2
Manuel Andrade <104844085+ManuelAndrade1@users.noreply.github.com>
2023-10-05 23:10:40 -0300

    Added Guia 6 files

--- GUIA_1/ej2_merge.c
+++ GUIA_1/ej2_merge.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void sort(int* arr, int size) {
+    for (int i = 0; i < size-1; i++) {
+        for (int j = i+1; j < size-1; j++) {
+            if(arr[j] < arr[i]) {
+                swap(&arr[j], &arr[i]);
+            }
+        }
+    }
+}
+
 int* merge(int* A, int sizeA, int* B, int sizeB) {
 
     int *res = (int*) malloc(sizeof(int) * (sizeA + sizeB));
@@ -13,7 +29,23 @@ int* merge(int* A, int sizeA, int* B, int sizeB) {
         pos += 1;
         res[pos] = B[j];
     }
+    sort(res, sizeA+sizeB);
     return res;
+}
+
+void swap_float(float* a, float* b) {
+    float temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void sort_float(float* arr, int size) {
+    for (int i = 0; i < size-1; i++) {
+        for (int j = i+1; j < size-1; j++) {
+            if(arr[j] < arr[i]) {
+                swap_float(&arr[j], &arr[i]);
+            }
+        }
+    }
 }
 
 float* merge_to_float(int* A, int sizeA, int* B, int sizeB) {
@@ -28,6 +60,7 @@ float* merge_to_float(int* A, int sizeA, int* B, int sizeB) {
         pos += 1;
         res[pos] = (float) B[j];
     }
+    sort_float(res, sizeA+sizeB);
     return res;
 }
 
